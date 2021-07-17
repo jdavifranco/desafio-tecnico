8782647851e1302606b8b968954b5a8746a26695
jdavifranco <developerjdavi@gmail.com>
2021-07-17 16:28:25 -0300

    add retrofit dependencies

---
+++ TokenFilmes/app/src/main/java/com/jdavifranco/desafio/tokenfilmes/network/FilmesApiService.kt
@@ -0,0 +1,11 @@
+package com.jdavifranco.desafio.tokenfilmes.network
+/*
+Interface que contém as chamadas para a api, essas chamadas serão feitas
+ utilizando a biblioteca retrofit.
+A biblioteca retrofit cria as chamadas http com base nos parâmetros
+passados: A Url Base e um ConverterFactory  que deve saber como converter
+os dados recebidos para objetos kotlin
+ */
+
+interface FilmesApiService {
+}
---
+++ TokenFilmes/app/src/main/java/com/jdavifranco/desafio/tokenfilmes/network/FilmesDTO.kt
@@ -0,0 +1,4 @@
+package com.jdavifranco.desafio.tokenfilmes.network
+
+class FilmesDTO {
+}
