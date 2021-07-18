43defc8a6757aed12aa9f740c6645ac56bf3615d
jdavifranco <developerjdavi@gmail.com>
2021-07-18 00:25:53 -0300

    add moshi converter and DTO class

--- TokenFilmes/app/src/main/java/com/jdavifranco/desafio/tokenfilmes/network/FilmesApiService.kt
+++ TokenFilmes/app/src/main/java/com/jdavifranco/desafio/tokenfilmes/network/FilmesApiService.kt
@@ -1,14 +1,15 @@
 package com.jdavifranco.desafio.tokenfilmes.network
 
-import retrofit2.Call
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
 /*
 Interface que contém as chamadas para a api, essas chamadas serão feitas
- utilizando a biblioteca retrofit.
+ utilizando a biblioteca retrofit: https://square.github.io/retrofit/
 A biblioteca retrofit cria as chamadas http com base nos parâmetros
 passados: A Url Base e um ConverterFactory  que deve saber como converter
 os dados recebidos para objetos kotlin
@@ -16,21 +17,35 @@ os dados recebidos para objetos kotlin
 //Apenas um método que requisita todos os filmes
 interface FilmesApiService {
     @GET("movies")
-    fun getFilmes(): Call<String>
-}
-//Base Url
-private const val BASE_URL = "https://desafio-mobile.nyc3.digitaloceanspaces.com/"
-//criando o objeto retrofit
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .baseUrl("https://api.github.com/")
-    .build()
+    suspend fun getFilmes(): List<FilmesNetwork>
 
-// public object que servirá para acesso a api no app
-object FilmesApi {
-    val retrofitService : FilmesApiService by lazy {
-        retrofit.create(FilmesApiService::class.java)
+    companion object{
+        /*
+        Create moshi object para ser usado pelo retrofit como converter
+        Moshi é uma biblioteca json que facilita a conversao de json para objetos java
+        https://github.com/square/moshi
+        */
+        private val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        //Base Url
+        private const val BASE_URL = "https://desafio-mobile.nyc3.digitaloceanspaces.com/"
+        //criando o objeto retrofit
+
+        private val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl("https://api.github.com/")
+            .build()
+
+        // retrofit service criado utlizando a interface FilmesApiService
+        val retrofitService : FilmesApiService by lazy {
+            retrofit.create(FilmesApiService::class.java)
+        }
+
     }
+
 }
+
 
 
--- TokenFilmes/app/src/main/java/com/jdavifranco/desafio/tokenfilmes/network/FilmesDTO.kt
+++ TokenFilmes/app/src/main/java/com/jdavifranco/desafio/tokenfilmes/network/FilmesDTO.kt
@@ -1,4 +1,25 @@
 package com.jdavifranco.desafio.tokenfilmes.network
 
-class FilmesDTO {
-}
+import com.squareup.moshi.Json
+/*
+Data transfer object, essa é uma classe cuja função é armazenar os dados recebidos
+através das chamadas http para api.
+id": 19404,
+      "vote_average": 9.3,
+      "title": "Dilwale Dulhania Le Jayenge",
+      "poster_url": "https://image.tmdb.org/t/p/w200/uC6TTUhPpQCmgldGyYveKRAu8JN.jpg",
+      "genres": [
+        "Comedy",
+        "Drama",
+        "Romance"
+      ],
+      "release_date": "1995-10-20"
+ */
+data class FilmesNetwork(
+    val id: String,
+    val vote_average:Double,
+    val title:String,
+    // used to map img_src from the JSON to imgSrcUrl in our class
+    @Json(name = "poster_src") val poster_url: String,
+    val genres: List<String>,
+    val release_date: String)
